246dbb0857bd195a4fd07a25add0170d99e34863
Rodrigo Gomes <159979512+rRodrigoGomes@users.noreply.github.com>
2026-06-07 19:36:25 -0300

    fix main_all_dataset-non-default
    Concatenate DataFrames for 'm' and 'b' and limit to 1000 rows.

--- src/paper/non_default_params/main_all_dataset.py
+++ src/paper/non_default_params/main_all_dataset.py
@@ -8,8 +8,8 @@ def main() -> None:
     experiments.configure_logging(__file__)
     d, folder, template = setup_experiment()
 
-    m: list[pd.DataFrame] = [d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[32], d[33], d[34], d[35]]
-    b: list[pd.DataFrame] = [d[8], d[9], d[11], d[10], d[12], d[13], d[15], d[16], d[17], d[18], d[19], d[20], d[21], d[22], d[23], d[24], d[25], d[30], d[27], d[29]]
+    m: list[pd.DataFrame] = [pd.concat([d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[32], d[33], d[34], d[35]])[:1000]]
+    b: list[pd.DataFrame] = [pd.concat([d[8], d[9], d[11], d[10], d[12], d[13], d[15], d[16], d[17], d[18], d[19], d[20], d[21], d[22], d[23], d[24], d[25], d[30], d[27], d[29]])[:1000]]
     run_dataset("all_combined_s0", m, b, folder, template)
 
 
