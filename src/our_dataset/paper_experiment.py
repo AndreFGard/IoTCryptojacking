f5b930f768097f624912060efeac29f223ae5f9d
Andrefgard <96950645+AndreFGard@users.noreply.github.com>
2026-06-15 14:54:07 -0300

    create separate models file

--- src/our_dataset/paper_experiment.py
+++ src/our_dataset/paper_experiment.py
@@ -9,88 +9,13 @@ import pandas as pd
 from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-from joblib import Parallel, delayed
 
 from our_dataset import dataset, transforms
 from our_dataset.tuning import tune_model
-
+from our_dataset.models import SVCFactory, RandomForestFactory
 dataset_df: pd.DataFrame | None = None
 
 
-class SVCFactory:
-    name = "SVC"
-    def __init__(self):
-        self.param_grids = [
-            {
-                "C": [1, 2],
-                "kernel": ["linear"],
-                "gamma": ["scale"],
-                "class_weight": ["balanced", None],
-            },
-            {
-                "C": [1, 2],
-                "kernel": ["poly", "rbf", "sigmoid"],
-                "gamma": ["scale", "auto"],
-                "class_weight": ["balanced", None],
-            },
-        ]
-
-    def make_model(self, **params):
-        C = params["C"]
-        kernel = params["kernel"]
-        class_weight = params.get("class_weight", None)
-        gamma = params.get("gamma", "scale")
-        comb_name = f"SVC - {C} - {kernel} - {gamma} - {class_weight}"
-
-        if kernel == "linear":
-            model = LinearSVC(
-                C=C,
-                loss="hinge",
-                class_weight=class_weight,
-                random_state=42,
-                dual=True,
-                max_iter=10000,
-            )
-        else:
-            model = SVC(
-                C=C,
-                kernel=kernel,
-                gamma=gamma,
-                class_weight=class_weight,
-                random_state=42,
-            )
-        return self.name, comb_name, model
-
-
-class RandomForestFactory:
-    name = "RandomForest"
-    def __init__(self):
-        self.param_grids = [
-            {
-                "n_estimators": [50, 100, 200],
-                "max_depth": [None, 10,],
-                "min_samples_split": [2, 5],
-                "class_weight": ["balanced_subsample", None],
-            }
-        ]
-
-    def make_model(self, **params):
-        n_estimators = params["n_estimators"]
-        max_depth = params["max_depth"]
-        min_samples_split = params["min_samples_split"]
-        class_weight = params.get("class_weight", None)
-        
-        comb_name = f"RF - {n_estimators} - {max_depth} - {min_samples_split} - {class_weight}"
-        
-        model = RandomForestClassifier(
-            n_estimators=n_estimators,
-            max_depth=max_depth,
-            min_samples_split=min_samples_split,
-            class_weight=class_weight,
-            random_state=42,
-            n_jobs=-1
-        )
-        return self.name, comb_name, model
 
 
 def configure_logging() -> None:
